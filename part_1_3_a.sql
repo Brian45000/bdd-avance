63a9a85f08f4331069560afa27d51ceab4661107
Kevin Chauvet <kevin.chauvet@livecampus.tech>
2023-11-16 16:45:57 +0100

    Very_active_user_may

--- part_1_3_a.sql
+++ part_1_3_a.sql
@@ -1,4 +1,4 @@
 CREATE VIEW session_and_user AS
 SELECT s.connected_at, u.firstname, u.lastname, u.email, u.username, u.created_at 
 FROM session_table s
-INNER JOIN user_table u ON s.user_id = u.user_id
+INNER JOIN user_table u ON s.user_id = u.user_id;
--- part_1_3_b_view.sql
+++ part_1_3_b_view.sql
@@ -1,4 +1,4 @@
 CREATE VIEW session_per_month AS
 SELECT count(*), connected_at_month
 FROM session_table
-GROUP BY connected_at_month
+GROUP BY connected_at_month;
